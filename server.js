d368b7832e5ee9eebd0c8f2f98919cc1964457e5
Hemanth Vundavilli <118917390+HemanthVundavilli@users.noreply.github.com>
2025-10-14 14:36:33 +0530

    server.js

--- server.js
+++ server.js
@@ -29,16 +29,11 @@ app.use('/api/admins', adminRoutes);
 app.use('/api/students', studentRoutes);
 app.use('/api/teachers', teacherRoutes);
 
-
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected:', process.env.MONGO_URI))
   .catch(err => console.error('MongoDB connection error:', err));
 
-const PORT = process.env.PORT || 10000;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
-app.listen(PORT, '0.0.0.0', () => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
