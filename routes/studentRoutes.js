bfcf673df07cc6f49a6e7d6fdb404122125524ac
Hemanth Vundavilli <hemanthvundavilli@gmail.com>
2025-10-17 16:38:03 +0530

    first commit

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -7,13 +7,13 @@ const studentController = require('../controllers/studentController');
 router.get('/', authMiddleware(['admin', 'teacher']), studentController.getAllStudents);
 // router.get('/:id', authMiddleware(['admin', 'teacher', 'student']), studentController.getStudentDetails);
 
-router.put('/:id', authMiddleware(['admin']), adminController.updateStudent);
+// router.put('/:id', authMiddleware(['admin']), adminController.updateStudent);
 
 router.get('/:id', authMiddleware(['student']), studentController.getMyDetails);
 
 router.put('/marks/:id', authMiddleware(['teacher', 'admin']), studentController.updateMarks);
 router.put('/attendance/:id', authMiddleware(['teacher', 'admin']), studentController.updateAttendance);
-router.put('/admin-edit/:id', authMiddleware(['admin', 'teacher']), studentController.adminUpdateStudent);
+// router.put('/admin-edit/:id', authMiddleware(['admin', 'teacher']), studentController.adminUpdateStudent);
 router.post('/', authMiddleware(['admin', 'teacher']), studentController.createStudent);
 
 router.put('/students/:id', authMiddleware(['admin', 'teacher']), studentController.updateStudent);
